f29e175b0fc6d40c3acb4607bf9d4d5023ac2cb7
Atuoha <atuohainitiatives@gmail.com>
2023-05-15 13:21:10 +0100

    chats bug fix

--- lib/business_logic/open_ai_completions/open_ai_completions_state.dart
+++ lib/business_logic/open_ai_completions/open_ai_completions_state.dart
@@ -36,7 +36,7 @@ class OpenAiCompletionsState extends Equatable {
   }) {
     return OpenAiCompletionsState(
       completions: completions ?? this.completions,
-      chats: completions ?? this.chats,
+      chats: chats ?? this.chats,
       currentMessage: currentMessage ?? this.currentMessage,
     );
   }
